626953d975fea29383996ef06054a5f0d15df2f0
David Peter Gomes <david.peter.gomes@gmail.com>
2025-07-04 12:17:20 +0530

    Add files via upload

---
+++ Calender for the inputyr.py
@@ -0,0 +1,9 @@
+import calendar
+try:
+  year=int(input("Enter the year(e.g,2025):")) #e.g is like an example for customer as to how to input
+
+
+except ValueError: # means ki uper integer nahi input kuya to error
+  print("invalid input, enter a valid numeric figure")
+  exit()
+print(calendar.calendar(year))
